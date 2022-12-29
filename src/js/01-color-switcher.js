1677bc5ef0026fe324f97f478931684ce9c6c03c
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2022-12-29 15:30:41 +0200

    first js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bcgColor = document.querySelector('body');
+
+let timeOutId = null;
+let isActive = false;
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStart(evt) {
+    if (isActive) {
+        return;
+    }
+  timeOutId = setInterval(()=>{
+         isActive = true;     
+    bcgColor.style.backgroundColor = getRandomHexColor();
+
+},1000)
+};
+
+function onClickStop(evt) {
+    // bcgColor.style.backgroundColor = '#fafafa';
+    clearInterval(timeOutId);
+    isActive = false;
+}; 
+
+
 
